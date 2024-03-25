3f72c20ea1eeaf4116590fc319e35045255746b6
i.chapaev <i.chapaev@smartway.today>
2024-03-25 03:24:42 +0300

    commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+import uvicorn
+
+from fastapi import FastAPI, UploadFile
+
+import showViolationImage
+
+#http://92.53.97.223:8081
+apiUrl = "http://localhost:5122/files/"
+
+app = FastAPI()
+
+
+@app.post("/detect")
+async def detect(file:UploadFile,  token:str):
+    filename = f"requests_files/{file.filename}"
+
+    with open(filename, 'wb') as f:
+        f.write(await file.read())
+
+
+    violations = showViolationImage.isViolationImage(filename, file.filename)
+    file_link = post_file(f"response_files/{file.filename}/{file.filename}", file.content_type, token)
+    return {"violationTypes": violations, "fileLink":file_link}
+
+def post_file(filename, content_type, token):
+    files = {'formFile': ("filename1234", open(filename, 'rb'))}
+    headers = {'Authorization': token}
+    id = requests.post(apiUrl, headers=headers, files=files).json()
+    print(id)
+
+    return id
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
